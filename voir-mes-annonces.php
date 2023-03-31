4fff435e4ca63f04aa6baf2c08b749dfd61a74f6
Rahelison <dollyraheliarisoa@gmail.com>
2023-03-31 08:05:06 +0200

    nouveau fichier

--- voir-mes-annonces.php
+++ voir-mes-annonces.php
@@ -1,18 +1,2 @@
-<?php require_once('testAdmin.php') ?>
-<?php require_once('bib/header.inc.php') ?>
-<h1>Les livres <a href="productAdd.php">Ajouter</a></h1>
-<?php 
-//préparer une requete pour récupérer TOUS les users
-$query = "SELECT * FROM livres ORDER BY titre ASC";
-
-$result = $connectPDO->query($query);
-
-if($result->rowCount()>0){
-
-echo 'ok';
-}else{
-echo '<h3>Aucun livre dans la base de données</h3>';
-}
-
-
-require_once('bib/footer.inc.php') ?>
+<?php
+?>
